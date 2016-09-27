72af88f46b70592e0f2ccb2bf6d0e3d0d118739e
Jason  Sands <jasonsands@Macintosh.local>
2016-09-27 13:24:52 -0400

    3rd commit

--- AIhw/src/DPLL.java
+++ AIhw/src/DPLL.java
@@ -1,4 +1,13 @@
+import java.util.ArrayList;
+import java.util.Vector;
 
 public class DPLL {
-
+	
+	void dpll(ArrayList<Vector<Integer>> clauseList){
+		boolean sat = false;
+		// Check for consistent set of literals
+		for (int i = 0; i < clauseList.size(); i++) {
+			
+		}
+	}
 }
